20be3cd9f3738721d4b69e68ccb28bce7ea76c75
Matthew Vogee <mvogee@yahoo.com>
2017-06-13 17:19:24 -0700

    basic layout of play_game

--- playGame.java
+++ playGame.java
@@ -6,6 +6,27 @@ public class playGame
 	public static int play_game(Deck deck, ArrayList<String> playerDeck,
 					ArrayList<String> dealerDeck, int bet, int insurance)
 	{
-		return 1;
+		String usr;
+		Scanner userIn = new Scanner(System.in);
+		boolean playig = true;
+
+		System.out.println("It is your turn.");
+		while (playing)
+		{
+			System.out.println("Options:\nStand\nHit\nDouble\nSplit\nSurrender");
+			usr = userIn.nextLine();
+			if (usr.toLowerCase().equals("stand"))
+				stand(); // create this
+			else if (usr.toLowerCase().equals("hit"))
+				hit(); // create this
+			else if (usr.toLowerCase().equals("double"))
+				player_double(); // create this
+			else if (usr.toLowerCase().equals("split"))
+				split(); // create this
+			else if (usr.toLowerCase().equals("surrender"))
+				surrneder(); // creat this
+			else
+				System.out.println("I dont understand that play");
+		}
 	}
 }
